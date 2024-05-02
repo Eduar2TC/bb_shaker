4a097ce4ced0fb66a8f82bc4bb25f8b8118cd616
Eduar2tc <edua2tc@gmail.com>
2024-05-02 03:33:06 -0600

    add shake anim

--- lib/pages/custom_widgets/custom_layout_builder.dart
+++ lib/pages/custom_widgets/custom_layout_builder.dart
@@ -1,26 +1,33 @@
 import 'package:baby_shaker/pages/custom_widgets/shake_animation.dart';
 import 'package:flutter/material.dart';
 
-class CustomLayoutBuilder extends StatelessWidget {
+class CustomLayoutBuilder extends StatefulWidget {
   const CustomLayoutBuilder({
     super.key,
     required this.imagePath,
     required this.animationController,
   });
+
   final String imagePath;
   final AnimationController animationController;
 
+  @override
+  State<CustomLayoutBuilder> createState() => _CustomLayoutBuilderState();
+}
+
+class _CustomLayoutBuilderState extends State<CustomLayoutBuilder>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     const String tmpString = 'lib/assets/images/shake.png';
     return LayoutBuilder(
       builder: (context, constraints) {
-        return imagePath == tmpString
+        return widget.imagePath == tmpString
             ? ShakeAnimation(
-                animationController: animationController,
+                animationController: widget.animationController,
                 child: Image(
                   image: AssetImage(
-                    imagePath,
+                    widget.imagePath,
                   ),
                   width: constraints.maxWidth,
                   fit: BoxFit.scaleDown,
@@ -28,7 +35,7 @@ class CustomLayoutBuilder extends StatelessWidget {
               )
             : Image(
                 image: AssetImage(
-                  imagePath,
+                  widget.imagePath,
                 ),
                 width: constraints.maxWidth,
                 fit: BoxFit.cover,
