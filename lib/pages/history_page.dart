29bbd0726ca36b37d2798fbd067ddfcd77e1a556
Eduar2tc <edua2tc@gmail.com>
2024-05-17 00:54:13 -0600

    status bar

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,5 +1,6 @@
 import 'package:baby_shaker/Utils/game_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HistoryPage extends StatelessWidget {
   HistoryPage({super.key});
@@ -8,110 +9,108 @@ class HistoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool verticalDragged = false;
-    return Theme(
-      data: Theme.of(context).copyWith(
-        colorScheme: Theme.of(context).colorScheme,
-        iconTheme: Theme.of(context).iconTheme,
-        appBarTheme: Theme.of(context).appBarTheme,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.blue,
+        statusBarBrightness: Brightness.light,
       ),
-      child: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        appBar: verticalDragged == false
-            ? AppBar(
-                automaticallyImplyLeading: true,
-                title: const Text(
-                  'History',
-                ),
-              )
-            : null,
-        body: NotificationListener<ScrollNotification>(
-          onNotification: (scrollNotification) {
-            if (scrollNotification is ScrollStartNotification) {
-              verticalDragged = true;
-            } else if (scrollNotification is ScrollEndNotification) {
-              verticalDragged = false;
-            }
-            return true;
-          },
-          child: Column(
-            children: [
-              Expanded(
-                child: FutureBuilder<List<String>>(
-                  future: gameData.getGameData('gameData'),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<String>> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CircularProgressIndicator();
-                    } else {
-                      if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme: Theme.of(context).colorScheme,
+          iconTheme: Theme.of(context).iconTheme,
+          appBarTheme: Theme.of(context).appBarTheme,
+        ),
+        child: Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          appBar: /*AppBar(
+            title: const Text(
+              'History',
+            ),
+          )*/
+              null,
+          body: SafeArea(
+            child: Column(
+              children: [
+                Expanded(
+                  child: FutureBuilder<List<String>>(
+                    future: gameData.getGameData('gameData'),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<String>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
                       } else {
-                        if (snapshot.data!.isEmpty) {
-                          return Center(
-                            child: Container(
-                              padding: const EdgeInsets.all(100),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Text(
-                                style: TextStyle(
-                                  color: Colors.black.withOpacity(0.6),
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                'No data found',
-                              ),
-                            ),
-                          );
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
                         } else {
-                          List<String> sortedData =
-                              List<String>.from(snapshot.data!);
-                          sortedData.sort((a, b) => a.compareTo(b));
-                          String smallestItem = sortedData.first;
+                          if (snapshot.data!.isEmpty) {
+                            return Center(
+                              child: Container(
+                                padding: const EdgeInsets.all(100),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text(
+                                  style: TextStyle(
+                                    color: Colors.black.withOpacity(0.6),
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  'No data found',
+                                ),
+                              ),
+                            );
+                          } else {
+                            List<String> sortedData =
+                                List<String>.from(snapshot.data!);
+                            sortedData.sort((a, b) => a.compareTo(b));
+                            String smallestItem = sortedData.first;
 
-                          return CustomScrollView(
-                            slivers: [
-                              SliverAppBar(
-                                backgroundColor: Theme.of(context)
-                                    .appBarTheme
-                                    .backgroundColor,
-                                title: Text(
-                                  'Best time',
-                                  style: Theme.of(context).textTheme.bodyLarge,
+                            return CustomScrollView(
+                              slivers: [
+                                SliverAppBar(
+                                  pinned: true,
+                                  floating: true,
+                                  centerTitle: true,
+                                  //automaticallyImplyLeading: false,
+                                  backgroundColor: Theme.of(context)
+                                      .appBarTheme
+                                      .backgroundColor,
+                                  title: Text(
+                                    'Best time',
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge,
+                                  ),
                                 ),
-                                centerTitle: true,
-                                pinned: false,
-                                automaticallyImplyLeading: false,
-                              ),
-                              SliverList(
-                                delegate: SliverChildBuilderDelegate(
-                                  (context, index) {
-                                    return ListTile(
-                                      tileColor: Colors.white,
-                                      leading: const Icon(Icons.access_time),
-                                      trailing:
-                                          snapshot.data![index] == smallestItem
-                                              ? const Icon(Icons.star)
-                                              : null,
-                                      title: Text(snapshot.data![index]),
-                                      style:
-                                          Theme.of(context).listTileTheme.style,
-                                    );
-                                  },
-                                  childCount: snapshot.data!.length,
+                                SliverList(
+                                  delegate: SliverChildBuilderDelegate(
+                                    (context, index) {
+                                      return ListTile(
+                                        tileColor: Colors.white,
+                                        leading: const Icon(Icons.access_time),
+                                        trailing: snapshot.data![index] ==
+                                                smallestItem
+                                            ? const Icon(Icons.star)
+                                            : null,
+                                        title: Text(snapshot.data![index]),
+                                        style: Theme.of(context)
+                                            .listTileTheme
+                                            .style,
+                                      );
+                                    },
+                                    childCount: snapshot.data!.length,
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
+                              ],
+                            );
+                          }
                         }
                       }
-                    }
-                  },
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
